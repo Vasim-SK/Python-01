6996c5f40857d5f1685e3f4dd594415502fb3eb0
vasim <vasimabdulshaik@gmail.com>
2026-07-10 17:30:15 +0530

    Simple Calculator

---
+++ simple_calculator.py
@@ -0,0 +1,9 @@
+# Simple calculator (add +, sub -, multiply *, div /)
+
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter a Number: "))
+
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("Multiplication =", num1 * num2)
+print("Division =", num1 / num2)
