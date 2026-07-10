cd52f3e4fa9ab42b8c80a865cb646ff1dc88f44f
vasim <vasimabdulshaik@gmail.com>
2026-07-10 22:53:35 +0530

    Star Pattren

--- star_pattren.py
+++ star_pattren.py
@@ -1,4 +1,20 @@
+# To print Star Pattren in Square 
+
 for i in range(1, 6):
     for j in range(1, 6):
         print("*", end = " ")
     print()
+
+
+# To print Star pattren in triangle 
+
+for i in range(1, 6):
+    print("*" * i)
+
+
+# To print Pyramid 
+
+n = 5 
+
+for i in range(1, n + 1):
+    print(" " * (n - i) + " * " * i)
