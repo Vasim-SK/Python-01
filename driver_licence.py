9054fecfb9ee1ce264d29d17c0b21b9357a21896
vasim <vasimabdulshaik@gmail.com>
2026-07-10 22:10:39 +0530

    Driving licence eligablility

---
+++ driver_licence.py
@@ -0,0 +1,7 @@
+name = input("Enter Your Name: ")
+age = int(input("Enter Your age: "))
+
+if age >= 18:
+    print(f"Hi {name}, you are eligable for licence")
+else:
+    print(f"Hi {name}, you are not eligable for licence")
