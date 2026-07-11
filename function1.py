e008765933b440c27542677a623c391bad79923b
vasim <vasimabdulshaik@gmail.com>
2026-07-11 21:38:00 +0530

    Functions

---
+++ function1.py
@@ -0,0 +1,12 @@
+def student(name, age, city):
+    return name, age, city
+obj = student("Vasim", 22, "Hyderabad")
+print(obj)
+
+# Another 
+
+def student(name, age, city):
+    print("Name = ", name)
+    print("Age = ", age)
+    print("City = ", city)
+student("Vasim", 22, "Hyderabad")
